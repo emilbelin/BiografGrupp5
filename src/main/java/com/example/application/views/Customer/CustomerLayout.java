a667a37600639dcdb0b6eb019c9a139ba71eaa46
EmilB <emilbelin@live.se>
2021-04-16 01:30:42 +0200

    Går ej att boka föreställning med noll platser. Startsida knapp.

--- src/main/java/com/example/application/views/Customer/CustomerLayout.java
+++ src/main/java/com/example/application/views/Customer/CustomerLayout.java
@@ -1,7 +1,9 @@
 package com.example.application.views.Customer;
 
+import com.vaadin.flow.component.UI;
 import com.vaadin.flow.component.applayout.AppLayout;
 import com.vaadin.flow.component.applayout.DrawerToggle;
+import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.html.H1;
 import com.vaadin.flow.component.orderedlayout.FlexComponent;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
@@ -17,14 +19,18 @@ public class CustomerLayout extends AppLayout {
     {
         H1 logo = new H1("Bokning");
         logo.addClassName("logo");
-
-        HorizontalLayout header = new HorizontalLayout(new DrawerToggle(), logo);
-
-        header.setDefaultVerticalComponentAlignment(FlexComponent.Alignment.CENTER);
-        header.setWidth("100%");
+        Button backButton = new Button("Gå till Startsidan");
+        backButton.addClickListener(event -> UI.getCurrent().navigate(""));
+        HorizontalLayout header = new HorizontalLayout(logo);
+        HorizontalLayout button = new HorizontalLayout(backButton);
+        backButton.setWidth("100%");
+        backButton.setHeight("100%");
+        header.setVerticalComponentAlignment(FlexComponent.Alignment.CENTER);
+        header.setAlignItems(FlexComponent.Alignment.CENTER);
+        header.setWidth("90%");
         header.addClassName("header");
 
-        addToNavbar(header);
+        addToNavbar(header, button);
     }
 
 
